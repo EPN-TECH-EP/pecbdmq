45ff730cb999a9b2387ed0142c7fee8299cdad3f
JG <ludwing.gomez@epn.edu.ec>
2023-03-14 10:00:00 -0500

    feat: boton agregar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://192.168.0.184:8081'
+   apiUrl: 'http://192.168.0.184:8081'
   //apiUrl: 'http://localhost:8084'
-  apiUrl: 'http://192.168.68.69:8081'
+  //apiUrl: 'http://192.168.68.69:8081'
 };
 
 /*
