08b4818574cc4e9b619c610e08cea817c72a479a
Diego Moreno Rosero <dmoreno@tech.epn.edu.ec>
2023-07-31 11:21:39 -0500

    cambio endpoint tipoFalta env a gateway

--- src/app/servicios/tipo-falta.service.ts
+++ src/app/servicios/tipo-falta.service.ts
@@ -15,19 +15,19 @@ export class TipoFaltaService {
   constructor(private http: HttpClient) { }
 
   getTiposFalta(): Observable<ITipoFalta[]>{
-    return this.http.get<ITipoFalta[]>(`${this.host}/gen_tipo_falta/listar`);
+    return this.http.get<ITipoFalta[]>(`${this.host}/tipoFalta/listar`);
   }
 
   createTipoFalta(tipoFalta: ITipoFalta): Observable<HttpResponse<ITipoFalta>>{
-    return this.http.post<ITipoFalta>(`${this.host}/gen_tipo_falta/crear`, tipoFalta, { observe: 'response' });
+    return this.http.post<ITipoFalta>(`${this.host}/tipoFalta/crear`, tipoFalta, { observe: 'response' });
   }
 
   updateTipoFalta(tipoFalta: ITipoFalta, tipoFaltaId: number): Observable<HttpResponse<ITipoFalta>>{
-    return this.http.put<ITipoFalta>(`${this.host}/gen_tipo_falta/${tipoFaltaId}`, tipoFalta, { observe: 'response' });
+    return this.http.put<ITipoFalta>(`${this.host}/tipoFalta/${tipoFaltaId}`, tipoFalta, { observe: 'response' });
   }
 
   deleteTipoFalta(cod_tipo_falta: number): Observable<string>{
-    return this.http.delete<string>(`${this.host}/gen_tipo_falta/${cod_tipo_falta}`);
+    return this.http.delete<string>(`${this.host}/tipoFalta/${cod_tipo_falta}`);
   }
 
   listarTipoFaltaPeriodo(){
