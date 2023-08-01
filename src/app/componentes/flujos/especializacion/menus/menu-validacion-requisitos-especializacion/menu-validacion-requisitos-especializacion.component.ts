7002f2671103a9688ca60322b170c707e2ec335e
Pedro <fernandoaponte1103@gmail.com>
2023-08-01 14:47:34 -0500

    Add menus especializacion

---
+++ src/app/componentes/flujos/especializacion/menus/menu-validacion-requisitos-especializacion/menu-validacion-requisitos-especializacion.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from "../../../../../servicios/menu.service";
+import { Router } from "@angular/router";
+import { Submenu } from "../../../../../modelo/util/submenu";
+
+@Component({
+  selector: 'app-menu-validacion-requisitos',
+  templateUrl: './menu-validacion-requisitos.component.html',
+  styleUrls: ['./menu-validacion-requisitos.component.scss']
+})
+export class MenuValidacionRequisitosComponent extends Submenu implements OnInit {
+
+  constructor(private menuService: MenuService, private router: Router) {
+    super();
+  }
+
+  ngOnInit(): void {
+    super.initMenu(this.menuService, this.router);
+  }
+}
