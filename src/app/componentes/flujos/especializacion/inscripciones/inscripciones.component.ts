7589eea123d9e365839015f24998ce48060f2d95
Pedro <fernandoaponte1103@gmail.com>
2023-08-01 06:01:49 -0500

    Se agrega estructura inicial para inscripciones y validacion en especializacion

---
+++ src/app/componentes/flujos/especializacion/inscripciones/inscripciones.component.ts
@@ -0,0 +1,164 @@
+import { Component, OnInit } from '@angular/core';
+import { InscripcionItem } from "../../../../modelo/flujos/formacion/inscripcion-item";
+import { ValidacionInscripcionService } from "../../../../servicios/formacion/validacion-inscripcion.service";
+import { DatoPersonalService } from "../../../../servicios/dato-personal.service";
+import { Router } from "@angular/router";
+import { Usuario } from "../../../../modelo/admin/usuario";
+import { AutenticacionService } from "../../../../servicios/autenticacion.service";
+import { UsuarioAsignado } from "../../../../modelo/flujos/formacion/asignar-usuario";
+import { Notificacion } from "../../../../util/notificacion";
+import { MdbNotificationService } from "mdb-angular-ui-kit/notification";
+import { TipoAlerta } from "../../../../enum/tipo-alerta";
+import { FormacionService } from "../../../../servicios/formacion/formacion.service";
+import { catchError } from "rxjs/operators";
+import { HttpErrorResponse } from "@angular/common/http";
+import { of } from "rxjs";
+import { FORMACION } from "../../../../util/constantes/fomacion.const";
+import { DelegadoService } from "../../../../servicios/formacion/delegado.service";
+import { MuestraService } from "../../../../servicios/formacion/muestra.service";
+
+@Component({
+  selector: 'app-inscripciones',
+  templateUrl: './inscripciones.component.html',
+  styleUrls: ['./inscripciones.component.scss']
+})
+export class InscripcionesComponent implements OnInit {
+
+  usuario: Usuario = null;
+  inscripciones: InscripcionItem[]
+  inscripcionesAsignadas: InscripcionItem[]
+  inscripcionesLoaded = false
+  esEstadoValidacion = false
+  esEstadoMuestreo = false
+
+  headers = [
+    { key: 'id', label: 'ID' },
+    { key: 'cedula', label: 'Cédula' },
+    { key: 'nombre', label: 'Nombre' },
+    { key: 'apellido', label: 'Apellido' },
+  ]
+
+  constructor(
+    private validacionInscripcionService: ValidacionInscripcionService,
+    private datoPersonalService: DatoPersonalService,
+    private router: Router,
+    private autenticacionService: AutenticacionService,
+    private mdbNotificationService: MdbNotificationService,
+    private formacionService: FormacionService,
+    private delegadoService: DelegadoService,
+    private muestraService: MuestraService
+  ) {
+    this.inscripcionesAsignadas = []
+    this.inscripciones = []
+    this.autenticacionService.user$.subscribe({
+      next: usuario => {
+        this.usuario = usuario
+      }
+    })
+
+  }
+
+  ngOnInit(): void {
+
+    this.delegadoService.esDelegado(this.usuario.codUsuario).subscribe({
+      next: esDelegado => {
+        if (!esDelegado) {
+          Notificacion.notificar(this.mdbNotificationService, "No es usuario delegado", TipoAlerta.ALERTA_WARNING)
+          this.router.navigate(['/principal/formacion/menu-validacion'])
+          return
+        }
+      },
+      error: () => {
+        Notificacion.notificar(this.mdbNotificationService, "Ocurrió un error, inténtelo nuevamente", TipoAlerta.ALERTA_ERROR)
+        this.router.navigate(['/principal/especializacion/menu-validacion'])
+        return
+      }
+    })
+
+    this.formacionService.getEstadoActual().pipe(
+      catchError((errorResponse: HttpErrorResponse) => {
+        console.error(errorResponse)
+        return of(null);
+      })
+    ).subscribe({
+      next: estado => {
+
+        if (!estado || estado.httpStatusCode !== 200) {
+          Notificacion.notificar(this.mdbNotificationService, "No se pudo obtener el estado actual", TipoAlerta.ALERTA_WARNING)
+          this.router.navigate(['/principal/especializacion/menu-validacion'])
+          return;
+        }
+
+        if (estado.mensaje === FORMACION.estadoValidacion) {
+
+          this.esEstadoValidacion = true
+
+          this.validacionInscripcionService.listarInscripcionesByIdUsuario(this.usuario.codUsuario).subscribe({
+            next: inscripciones => {
+              console.log(inscripciones)
+              this.inscripcionesAsignadas = inscripciones.filter(inscripcion => inscripcion.estado === 'ASIGNADO')
+              this.inscripciones = inscripciones.filter(inscripcion => inscripcion.estado === 'PENDIENTE')
+              this.inscripcionesLoaded = true
+            },
+            error: err => console.log(err)
+          });
+        }
+
+        if (estado.mensaje === FORMACION.estadoMuestreo) {
+
+          this.esEstadoMuestreo = true
+
+          this.muestraService.listarByIdUsuario(this.usuario.codUsuario).subscribe({
+            next: muestras => {
+              console.log(muestras)
+              this.inscripcionesAsignadas = muestras.filter(inscripcion => inscripcion.estado === 'ASIGNADO MUESTRA');
+              this.inscripciones = muestras.filter(inscripcion => inscripcion.estado === 'MUESTRA');
+              this.inscripcionesLoaded = true
+            }
+          })
+        }
+      }
+    })
+
+
+  }
+
+  validar(inscripcion: InscripcionItem) {
+    this.validacionInscripcionService.idPostulante = inscripcion.codPostulante;
+    this.router.navigate(['principal/especializacion/validacion']).then()
+  }
+
+  validarMuestra(inscripcion: InscripcionItem) {
+    this.muestraService.idMuestra = inscripcion.codPostulante;
+    console.log(this,this.muestraService.idMuestra)
+    this.router.navigate(['principal/especializacion/muestra']).then()
+  }
+
+  asignar(idPostulante: number) {
+    const usuarioAsignado: UsuarioAsignado = {
+      codPostulante: idPostulante,
+      codUsuario: this.usuario.codUsuario,
+    }
+    this.validacionInscripcionService.asignarValidador(usuarioAsignado).subscribe({
+      next: (data) => {
+        console.log(data)
+        const index = this.inscripciones.findIndex(inscripcion => inscripcion.codPostulante === idPostulante);
+        if (index !== -1) {
+          const inscripcion = this.inscripciones.splice(index, 1)[0];
+          this.inscripcionesAsignadas.push(inscripcion);
+          this.inscripciones = this.inscripciones.filter(inscripcion => inscripcion.codPostulante !== idPostulante)
+        }
+        Notificacion.notificar(this.mdbNotificationService, "Usuario asignado correctamente", TipoAlerta.ALERTA_OK)
+      },
+      error: err => {
+        if (err.status !== 400) {
+          Notificacion.notificar(this.mdbNotificationService, "Error al asignar usuario", TipoAlerta.ALERTA_ERROR)
+        }
+        Notificacion.notificar(this.mdbNotificationService, "El usuario ya se encuentra asignado", TipoAlerta.ALERTA_WARNING)
+        this.inscripciones = this.inscripciones.filter(inscripcion => inscripcion.codPostulante !== idPostulante)
+        console.error(err)
+      }
+    })
+  }
+
+}
---
+++ src/app/componentes/flujos/especializacion/validacion/validacion.component.ts
@@ -0,0 +1,153 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { ValidacionInscripcionService } from "../../../../servicios/formacion/validacion-inscripcion.service";
+import { ValidacionRequisito } from "../../../../modelo/flujos/formacion/requisito";
+import { Notificacion } from "../../../../util/notificacion";
+import { MdbNotificationService } from "mdb-angular-ui-kit/notification";
+import { TipoAlerta } from "../../../../enum/tipo-alerta";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { InscripcionCompleta } from "../../../../modelo/flujos/formacion/inscripcion-completa";
+import { DocumentosService } from "../../../../servicios/formacion/documentos.service";
+import { forkJoin } from "rxjs";
+import { SafeResourceUrl } from "@angular/platform-browser";
+
+@Component({
+  selector: 'app-validacion',
+  templateUrl: './validacion.component.html',
+  styleUrls: ['./validacion.component.scss']
+})
+export class ValidacionComponent implements OnInit {
+
+  postulanteId: number | null;
+  inscripcion: InscripcionCompleta | null;
+  requisitos: ValidacionRequisito[];
+  formularioRequisitos: FormGroup[];
+  loadInformation: boolean;
+  headers: string[];
+  mensajeBtnListaRequisitos: string;
+  estaExpandidoListaRequisitos: boolean = false;
+  urlsArchivo: {urlSafe: SafeResourceUrl, nombreArchivo: string}[];
+
+  constructor(
+    private validacionInscripcionService: ValidacionInscripcionService,
+    private documentosService: DocumentosService,
+    private mdbNotificationService: MdbNotificationService,
+    private builder: FormBuilder,
+    private router: Router
+  ) {
+    this.postulanteId = null;
+    this.inscripcion = null;
+    this.requisitos = [];
+    this.urlsArchivo = [];
+    this.formularioRequisitos = [];
+    this.loadInformation = false;
+    this.headers = ['Requisito', 'Cumple el requisito', 'Observaciones'];
+    this.mensajeBtnListaRequisitos = 'Posición inferior de la lista de requisitos';
+
+  }
+
+  ngOnInit(): void {
+
+    this.postulanteId = this.validacionInscripcionService.idPostulante;
+    if (!this.postulanteId) {
+      Notificacion.notificar(this.mdbNotificationService, "No se ha seleccionado un postulante", TipoAlerta.ALERTA_ERROR);
+      this.router.navigate(['principal/especializacion/inscripciones']).then();
+      return;
+        }
+
+    this.validacionInscripcionService.getInscripcion(this.postulanteId).subscribe({
+      next: inscripcion => {
+        console.log("inscripcion", inscripcion);
+        this.inscripcion = inscripcion;
+
+        const observables = this.inscripcion?.documentos?.map(documento =>
+          this.documentosService.visualizarArchivo(documento.codDocumento)
+          );
+
+        if (observables && observables.length > 0) {
+          forkJoin(observables).subscribe({
+            next: urls => {
+              this.urlsArchivo = urls.map((url, index) => ({
+                urlSafe: url,
+                nombreArchivo: this.inscripcion?.documentos[index]?.nombre
+              }));
+              console.log("urlsArchivo", this.urlsArchivo);
+            },
+            error: err => console.log("No se pudo obtener los archivos", err)
+          });
+        }
+
+        this.validacionInscripcionService.listarRequisitos(this.inscripcion?.codPostulante).subscribe({
+          next: requisitos => {
+            console.log("requisitos", requisitos);
+            this.requisitos = requisitos;
+            this.construirFormulario();
+            this.loadInformation = true;
+          },
+          error: err => console.log("No se pudo obtener los requisitos", err)
+        });
+      },
+      error: err => {
+        console.log("No se pudo obtener la inscripción", err);
+        this.router.navigate(['principal/formacion/inscripciones']);
+      }
+    });
+
+    this.validacionInscripcionService.idPostulante = null;
+
+  }
+
+  private construirFormulario() {
+
+    this.requisitos.forEach(requisito => {
+      const requisitoFormGroup = this.builder.group({
+        codRequisitos: [requisito?.codRequisitos],
+        codValidacion: [requisito?.codValidacion],
+        codPostulante: [requisito?.codPostulante],
+        nombre: [requisito?.nombreRequisito],
+        estado: [requisito?.estado],
+        observaciones: [requisito?.observaciones],
+        estadoMuestra: [requisito?.estadoMuestra],
+        observacionMuestra: [requisito?.observacionMuestra]
+      });
+
+      this.formularioRequisitos.push(requisitoFormGroup);
+    });
+    // observo los cambios en el formulario
+    this.formularioRequisitos.forEach(requisito => {
+      requisito.valueChanges.subscribe(value => {
+        console.log(value);
+      });
+    });
+  }
+
+  guardarRequisitos() {
+
+    const hayRequisitosTocados = this.formularioRequisitos.some(requisito => requisito.touched);
+    if (!hayRequisitosTocados) return;
+    // solo guardo los requisitos que han sido tocados
+    const requisitos: ValidacionRequisito[] = this.formularioRequisitos.filter(
+      requisito => (requisito.touched && requisito.valid)
+    ).map(requisito => requisito.value);
+    console.log(requisitos);
+
+    this.validacionInscripcionService.guardarRequisitos(requisitos).subscribe({
+      next: () => {
+        Notificacion.notificar(this.mdbNotificationService, 'Se guardaron los requisitos', TipoAlerta.ALERTA_INFO);
+        this.router.navigate(['principal/formacion/inscripciones']);
+      },
+      error: err => {
+        Notificacion.notificar(this.mdbNotificationService, 'No se pudo guardar los requisitos', TipoAlerta.ALERTA_ERROR);
+        console.log("No se pudo guardar los requisitos", err);
+    }
+    });
+
+  }
+
+
+  toggleListaRequisitos() {
+    this.estaExpandidoListaRequisitos = !this.estaExpandidoListaRequisitos;
+    this.mensajeBtnListaRequisitos = this.estaExpandidoListaRequisitos ? 'Posición lateral de la lista de requisitos' : 'Posición inferior de la lista de requisitos';
+  }
+
+}
