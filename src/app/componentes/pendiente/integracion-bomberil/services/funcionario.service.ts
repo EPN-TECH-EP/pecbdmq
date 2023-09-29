6f23a5ca6e510e8e75c01114e1a3a0347eb27bc2
Timoteo Camuendo <59524647+Timo-Teo@users.noreply.github.com>
2023-09-29 15:17:42 -0500

    feat: carga de reconocimientos

---
+++ src/app/componentes/pendiente/integracion-bomberil/services/funcionario.service.ts
@@ -0,0 +1,53 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+
+
+export interface Funcionario {
+  codFuncionario: number;
+  operativo: boolean;
+  fechaIngreso: Date;
+  agrupacion: string;
+  type: string;
+  pin: string;
+  nombres: string;
+  apellidos: string;
+  email: string
+}
+
+export interface DocumentoFuncionario {
+  codDocumentoFuncionario: number;
+  codFuncionario: number;
+  codDocumento: number;
+  esReconocimiento: boolean;
+  esSancion: boolean;
+  observacion: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FuncionarioService {
+
+  host = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+  listar() {
+    return this.http.get<Funcionario[]>(`${this.host}/funcionario/listar`);
+  }
+
+  listarDocumentosReconocimiento(codFuncionario) {
+    return this.http.get<DocumentoFuncionario[]>(`${this.host}/funcionarioDocumento/listarReconocimientosByFuncionario/${codFuncionario}`);
+  }
+
+  guardarDocumentoReconocimiento(data: FormData) {
+    return this.http.post(`${this.host}/funcionarioDocumento/crearFully`, data);
+  }
+
+  eliminarDocumentoReconocimiento(codDocumentoFuncionario) {
+    return this.http.delete(`${this.host}/funcionarioDocumento/${codDocumentoFuncionario}`);
+  }
+
+}
---
+++ src/app/componentes/pendiente/integracion-bomberil/util/modal-carga-reconocimiento/modal-carga-reconocimiento.component.ts
@@ -0,0 +1,121 @@
+import {Component, OnInit} from '@angular/core';
+import {MdbModalRef} from "mdb-angular-ui-kit/modal";
+import {Funcionario, FuncionarioService} from "../../services/funcionario.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MdbNotificationService} from "mdb-angular-ui-kit/notification";
+import {TipoAlerta} from "../../../../../enum/tipo-alerta";
+import {Notificacion} from "../../../../../util/notificacion";
+import {DocumentosService} from "../../../../../servicios/formacion/documentos.service";
+
+@Component({
+  selector: 'app-modal-carga-reconocimiento',
+  templateUrl: './modal-carga-reconocimiento.component.html',
+  styleUrls: ['./modal-carga-reconocimiento.component.scss']
+})
+export class ModalCargaReconocimientoComponent implements OnInit {
+
+  documentos: any[];
+  archivo: File | null = null;
+  funcionario: Funcionario
+  addRow: boolean = false;
+  documentoForm: FormGroup;
+  headers: { label: string; key: string; }[]
+
+  constructor(
+    public modalRef: MdbModalRef<ModalCargaReconocimientoComponent>,
+    private formBuilder: FormBuilder,
+    private funcionariosService: FuncionarioService,
+    private ns: MdbNotificationService,
+    private documentosService: DocumentosService) {
+    this.funcionario = null;
+    this.documentoForm = new FormGroup({});
+
+    this.construirFormulario();
+  }
+
+  ngOnInit(): void {
+    this.listarDocumentosReconocimiento(this.funcionario.codFuncionario);
+
+  }
+
+  private listarDocumentosReconocimiento(codFuncionario: number) {
+    this.funcionariosService.listarDocumentosReconocimiento(this.funcionario.codFuncionario).subscribe({
+      next: documentos => {
+        this.documentos = documentos;
+        console.log(this.documentos);
+      }
+    })
+  }
+
+  private construirFormulario() {
+    this.documentoForm = this.formBuilder.group({
+      documento: ['', Validators.required],
+      observacion: ['', Validators.required],
+    });
+  }
+
+  private mostrarNotificacion(mensaje: string, tipo: TipoAlerta) {
+    Notificacion.notificar(this.ns, mensaje, tipo);
+  }
+
+
+  cargarArchivo(event: any) {
+    this.archivo = event.target.files[0];
+  }
+
+  guardarArchivo() {
+    if (this.documentoForm.invalid) {
+      this.documentoForm.markAllAsTouched();
+    }
+
+    const formData = new FormData();
+
+    const data = {
+      codFuncionario: this.funcionario.codFuncionario,
+      esReconocimiento: true,
+      observacion: this.documentoForm.get('observacion')?.value
+    }
+
+    formData.append('datosFuncionarioDocumento', JSON.stringify(data));
+    formData.append('docs', this.archivo);
+
+    this.funcionariosService.guardarDocumentoReconocimiento(formData).subscribe({
+      next: response => {
+        this.addRow = false;
+        this.mostrarNotificacion('Documento guardado correctamente', TipoAlerta.ALERTA_OK)
+        console.log(response);
+        this.listarDocumentosReconocimiento(this.funcionario.codFuncionario);
+      }
+    })
+
+  }
+
+  descargarDocumento(codDocumentoFuncionario: number) {
+    this.documentosService.descargar(codDocumentoFuncionario).subscribe(
+      {
+        next: (data) => {
+          const blob = new Blob([data]/*, {type: 'application/pdf'}*/);
+          const url = window.URL.createObjectURL(blob);
+          const link = document.createElement('a');
+          const nombreDocumento = blob.type.split('/')[1];
+          link.href = url;
+          link.download = `${nombreDocumento || 'Documento'}.pdf`;
+          link.click();
+          window.URL.revokeObjectURL(url);
+        },
+        error: (error) => {
+          console.log('Error al descargar documento', error);
+          Notificacion.notificar(this.ns, 'Error al descargar documento', TipoAlerta.ALERTA_ERROR)
+        }
+      });
+  }
+
+  eliminarDocumento(codDocumentoFuncionario: any) {
+    this.funcionariosService.eliminarDocumentoReconocimiento(codDocumentoFuncionario).subscribe({
+      next: response => {
+        this.mostrarNotificacion('Documento eliminado correctamente', TipoAlerta.ALERTA_OK)
+        this.listarDocumentosReconocimiento(this.funcionario.codFuncionario);
+      }
+    });
+  }
+}
