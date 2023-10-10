438ccf4e8b96847072d5440a95acd1f62d1a2159
Timoteo Camuendo <timo_teo.c@hotmail.com>
2023-10-10 08:27:14 -0500

    feat: prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.0.184:8081', // proxy
+  apiUrl: '192.168.0.164:8083', // proxy
 
   APP_KEY: 'vQ9sdpG52a3Rm4LZYc8fDlNKjnWbHIXheU6YiAzS1VJ0o7OtTwgxqpukCFREz',
   NUMERO_REINTENTOS : 3,
